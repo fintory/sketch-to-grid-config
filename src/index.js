f459ad9d3fc2b88be4b4921d8cf88f2da2a48de4
Nick Schmidt <nick@kreativgebiet.com>
2020-04-24 13:24:39 +0200

    feat(layout): implement a small layout with tailwind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import fileUpload from 'express-fileupload'
+import extractGridConfiguration from './extract-grid-configuration'
 
-var app = express()
+const app = express()
+
+app.use(fileUpload())
+app.use(bodyParser.json())
 
 app.set('view engine', 'ejs')
 
@@ -8,8 +14,12 @@ app.get('/', function (req, res) {
   res.render('index');
 })
 
-app.post('generate', function (req, res) {
-  res.render('index', req.params)
+app.post('/generate', function (req, res) {
+  const configuration = extractGridConfiguration({ file: req.files.file.data })
+
+  res.render('index', {
+    generated_configuration: JSON.stringify(configuration, null, 2)
+  })
 })
 
 app.listen(1337)
